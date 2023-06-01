1227d9c6dfd9ec026b66fcaaf82a70c95f5cc3e3
chreden <4263940+chreden@users.noreply.github.com>
2023-06-01 08:25:50 +0100

    Stop all nebula rotation by default
    Stop the nebulas from rotating by default. They can have their own animations so they don't spin like they do currently.
    Closes #26

--- mmm_armada/Nebula_Internal.cpp
+++ mmm_armada/Nebula_Internal.cpp
@@ -3,75 +3,63 @@
 
 namespace mmm
 {
-	NebulaPtr
-	Nebula::create( types::Entity* entity )
-	{
-		return NebulaPtr( new Nebula( static_cast<types::Nebula*>( entity ) ) );
-	}
+    NebulaPtr Nebula::create(types::Entity* entity)
+    {
+        return NebulaPtr(new Nebula(static_cast<types::Nebula*>( entity )));
+    }
 
-	Nebula::Nebula( types::Nebula* nebula )
-		: AreaEffectObject( nebula )
-	{
+    Nebula::Nebula(types::Nebula* nebula)
+        : AreaEffectObject(nebula)
+    {
+    }
 
-	}
+    bool Nebula::getCanBuildInNebula() const
+    {
+        return getNebula()->m_canWeBuildInHere;
+    }
 
-	bool
-	Nebula::getCanBuildInNebula( ) const
-	{
-		return getNebula()->m_canWeBuildInHere;
-	}
+    void Nebula::setCanBuildInNebula(bool value)
+    {
+        getNebula()->m_canWeBuildInHere = value;
+    }
 
-	void
-	Nebula::setCanBuildInNebula( bool value )
-	{
-		getNebula()->m_canWeBuildInHere = value;
-	}
+    float Nebula::getDeltaRoll() const
+    {
+        return getNebula()->delta_roll;
+    }
 
-	float	
-	Nebula::getDeltaRoll( ) const
-	{
-		return getNebula()->delta_roll;
-	}
-		
-	float 
-	Nebula::getDeltaPitch( ) const
-	{
-		return getNebula()->delta_pitch;
-	}
-		
-	float
-	Nebula::getDeltaYaw() const
-	{
-		return getNebula()->delta_yaw;
-	}
+    float Nebula::getDeltaPitch() const
+    {
+        return getNebula()->delta_pitch;
+    }
 
-	void			
-	Nebula::setDeltaRoll( float value )
-	{
-		getNebula()->delta_roll = value;
-	}
-		
-	void			
-	Nebula::setDeltaPitch( float value )
-	{
-		getNebula()->delta_pitch = value;
-	}
-		
-	void			
-	Nebula::setDeltaYaw( float value )
-	{
-		getNebula()->delta_yaw = value;
-	}
+    float Nebula::getDeltaYaw() const
+    {
+        return getNebula()->delta_yaw;
+    }
 
-	types::Nebula*
-	Nebula::getNebula( ) const
-	{
-		return static_cast<types::Nebula*>( getEntity() );
-	}
+    void Nebula::setDeltaRoll(float value)
+    {
+        getNebula()->delta_roll = value;
+    }
 
-	void
-	Nebula::allocateReplacement( luabind::detail::object_rep* obj )
-	{
-		entity_allocate_replacement<Nebula>( obj, boost::static_pointer_cast<Nebula>( shared_from_this() ) );
-	}
+    void Nebula::setDeltaPitch(float value)
+    {
+        getNebula()->delta_pitch = value;
+    }
+
+    void Nebula::setDeltaYaw(float value)
+    {
+        getNebula()->delta_yaw = value;
+    }
+
+    types::Nebula* Nebula::getNebula() const
+    {
+        return static_cast<types::Nebula*>(getEntity());
+    }
+
+    void Nebula::allocateReplacement(luabind::detail::object_rep* obj)
+    {
+        entity_allocate_replacement<Nebula>(obj, boost::static_pointer_cast<Nebula>(shared_from_this()));
+    }
 }
